0019b6588d768c5470cb9d8049e1b41c195ed1ba
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2024-02-28 07:09:08 +0300

    initial commit
    libGDX generated project

---
+++ android/src/com/mygdx/meow/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.meow;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.meow.MeowGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MeowGame(), config);
+	}
+}
